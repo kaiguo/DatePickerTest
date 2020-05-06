1b54c603aace44dd28547ce1a9fdce979583ec80
Kai Guo <kaigu@microsoft.com>
2020-05-06 13:52:51 -0700

    Add test project

---
+++ windows/DatePickerTest/App.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+
+
+namespace winrt::DatePickerTest::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::DatePickerTest::implementation
+
+
